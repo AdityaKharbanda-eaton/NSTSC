3e1f0f64e7f5b39ca965074cf0dcfa99273ed1ee
Aditya Kharbanda <AdityaKharbanda@eaton.com>
2025-05-02 14:00:21 +0100

    Report time for preprocessing, training and testing

--- Codes/NSTSC_main.py
+++ Codes/NSTSC_main.py
@@ -10,7 +10,7 @@ from utils.train_utils import *
 import pickle
 import sys
 import os
-
+import time
 def main():
     """
     @brief Main function to train and evaluate the NSTSC model.
@@ -18,15 +18,23 @@ def main():
     # Get Dataset_name from system arguments or use default value
     Dataset_name = sys.argv[1] if len(sys.argv) > 1 else "Coffee"
     Max_epoch = int(sys.argv[2]) if len(sys.argv) > 2 else 10
-    print('Start Training ---' + str(Dataset_name) + ' ---dataset\n')
+    print(f"Using dataset: {Dataset_name}")
+    print(f"Max epoch: {Max_epoch}")
     dataset_path_ = "../UCRArchive_2018/"
     normalize_dataset = True
     #Max_epoch = 10
     # model training
+    preprocess_time_start = time.time()
     Xtrain_raw, ytrain_raw, Xval_raw, yval_raw, Xtest_raw, ytest_raw = Readdataset(dataset_path_, Dataset_name)
     Xtrain, Xval, Xtest = Multi_view(Xtrain_raw, Xval_raw, Xtest_raw)
     N, T = calculate_dataset_metrics(Xtrain)
+    preprocess_time_end = time.time()
+    print(f"Preprocessing time: {preprocess_time_end - preprocess_time_start:.2f} seconds")
+    print('Start Training ---' + str(Dataset_name) + ' ---dataset\n')
+    train_time_start = time.time()
     Tree = Train_model(Xtrain, Xval, ytrain_raw, yval_raw, epochs=Max_epoch, normalize_timeseries=normalize_dataset)
+    train_time_end = time.time()
+    print(f"Training time: {train_time_end - train_time_start:.2f} seconds")
     # Save the learned tree to a file in the Tree_Models folder
     path = f"../Tree_Models"
     output_path = os.path.join(path, f"{Dataset_name}_learned_tree.pkl")
@@ -35,7 +43,10 @@ def main():
     with open(output_path, "wb") as f:
         pickle.dump(Tree, f)
     # model testing
+    evaluate_time_start = time.time()
     testaccu = Evaluate_model(Tree, Xtest, ytest_raw)
+    evaluate_time_end = time.time()
+    print(f"Evaluation time: {evaluate_time_end - evaluate_time_start:.2f} seconds")
     print("Test accuracy for dataset {} is --- {}".format(Dataset_name, testaccu))
 
 
