d91d36796adb5d8ae6231d985b66d87f6c256036
Aditya Kharbanda <AdityaKharbanda@Eaton.com>
2025-05-26 10:26:27 +0100

    sys arg support and time reporting

--- Codes/NSTSC_main.py
+++ Codes/NSTSC_main.py
@@ -8,32 +8,44 @@ from Models_node import *
 from utils.datautils import *
 from utils.train_utils import *
 import pickle
-
+import sys
+import time
 
 def main():
     """
     @brief Main function to train and evaluate the NSTSC model.
     """
-    Dataset_name = "BeetleFly"
-    print('Start Training ---' + str(Dataset_name) + ' ---dataset\n')
+    if len(sys.argv) < 3:
+        print("Usage: python NSTSC_main.py <Dataset_name> <Max_epoch>")
+        sys.exit(1)
+    Dataset_name = sys.argv[1]
+    Max_epoch = int(sys.argv[2])
+    print("Start Training ---" + str(Dataset_name) + " ---dataset\n")
     dataset_path_ = "../UCRArchive_2018/"
     normalize_dataset = True
-    Max_epoch = 100
     # model training
     # Xtrain_raw, ytrain_raw, Xval_raw, yval_raw, Xtest_raw, ytest_raw = Readdataset(dataset_path_, Dataset_name)
     # Xtrain, Xval, Xtest = Multi_view(Xtrain_raw, Xval_raw, Xtest_raw)
-
+    start_time = time.time()
     Xtrain, ytrain, Xval, yval, Xtest, ytest = Readdataset(dataset_path_, Dataset_name)
-
     N, T = calculate_dataset_metrics(Xtrain)
+    end_time = time.time()
+    print("Preprocessing time: {:.2f} seconds".format(end_time - start_time))
     # Tree = Train_model(Xtrain, Xval, ytrain_raw, yval_raw, epochs=Max_epoch, normalize_timeseries=normalize_dataset)
+    training_start_time = time.time()
     Tree = Train_model(Xtrain, Xval, ytrain, yval, epochs=Max_epoch, normalize_timeseries=normalize_dataset)
-    with open(f"{Dataset_name}_model.pkl", "wb") as model_file:
+    training_end_time = time.time()
+    print("Training time: {:.2f} seconds".format(training_end_time - training_start_time))
+    with open(f"../Tree_Models/{Dataset_name}_model.pkl", "wb") as model_file:
         pickle.dump(Tree, model_file)
+        print(f"Model saved to ../Tree_Models/{Dataset_name}_model.pkl")
     # model testing
     # testaccu = Evaluate_model(Tree, Xtest, ytest_raw)
+    testing_start_time = time.time()
     testaccu = Evaluate_model(Tree, Xtest, ytest)
+    testing_end_time = time.time()
     print("Test accuracy for dataset {} is --- {}".format(Dataset_name, testaccu))
+    print("Testing time: {:.2f} seconds".format(testing_end_time - testing_start_time))
 
 
 if __name__ == "__main__":
